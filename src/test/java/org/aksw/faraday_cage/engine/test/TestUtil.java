cd08fff4503958aeb6c3933296baf016c861769e
Kevin Dreßler <kvndrsslr@gmail.com>
2019-05-28 12:49:50 +0200

    drop mockito dependency here

--- src/test/java/org/aksw/faraday_cage/engine/test/TestUtil.java
+++ src/test/java/org/aksw/faraday_cage/engine/test/TestUtil.java
@@ -1,11 +1,6 @@
 package org.aksw.faraday_cage.engine.test;
 
 import org.aksw.faraday_cage.engine.ExecutionNode;
-import org.aksw.faraday_cage.engine.PluginFactory;
-import org.aksw.faraday_cage.vocabulary.FCAGE;
-import org.pf4j.DefaultPluginManager;
-
-import static org.mockito.Mockito.mock;
 
 /**
  *
@@ -14,12 +9,5 @@ class TestUtil {
 
   interface ITestExecutionNode extends ExecutionNode<String> {}
 
-  static PluginFactory<ITestExecutionNode> testFactory
-    = new PluginFactory<>(ITestExecutionNode.class, new DefaultPluginManager(), FCAGE.ExecutionNode);
-
-  @SuppressWarnings("unchecked")
-  static <V extends ExecutionNode<T>, T> PluginFactory<V> mockFactory() {
-    return mock(PluginFactory.class);
-  }
 
 }
