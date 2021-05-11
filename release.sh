b45d845886d16e3f6117de80f3b92bfc7818e992
Kevin Dreßler <kvndrsslr@gmail.com>
2021-05-11 09:08:41 +0200

    preparing release 1.2.0

--- release.sh
+++ release.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 if [ $# -ne 2 ]; then
-  echo "Usage: $0 new-version new-snapshot-version"
+  echo "Usage: $0 new_version new_snapshot_version"
   exit 1
 fi
-new-version="$1"
-new-snapshot-version="$2"
+new_version="$1"
+new_snapshot_version="$2"
 tmpOut=$(mktemp /tmp/mvn-release-tmp-out.XXXXXX)
 mvn verify > "$tmpOut" 2>&1
 if [ $? -eq 0 ]; then
-  mvn versions:set -DnewVersion="${new-version}" > "$tmpOut" 2>&1 && \
+  mvn versions:set -DnewVersion="${new_version}" > "$tmpOut" 2>&1 && \
   mvn versions:commit >> "$tmpOut" 2>&1 && \
   git add . >> "$tmpOut" 2>&1 && \
-  git commit -m "preparing release ${new-version}" >> "$tmpOut" 2>&1 && \
-  git tag -a "${new-version}" -m "release ${new-version}" >> "$tmpOut" 2>&1 && \
-  mvn versions:set -DnewVersion="${new-snapshot-version}" >> "$tmpOut" 2>&1 && \
+  git commit -m "preparing release ${new_version}" >> "$tmpOut" 2>&1 && \
+  git tag -a "${new_version}" -m "release ${new_version}" >> "$tmpOut" 2>&1 && \
+  mvn versions:set -DnewVersion="${new_snapshot_version}" >> "$tmpOut" 2>&1 && \
   mvn versions:commit >> "$tmpOut" 2>&1 && \
   git add . >> "$tmpOut" 2>&1 && \
   git commit -m "preparing next development iteration" >> "$tmpOut" 2>&1
@@ -21,8 +21,8 @@ if [ $? -eq 0 ]; then
     echo "Problem with release: Nothing has been pushed yet! Please consult the logs and resolve the issues before pushing!"
     cat "$tmpOut"
   else
-    echo "Successfully authored release ${new-version}"
-    git push origin "${new-version}"
+    echo "Successfully authored release ${new_version}"
+    git push origin "${new_version}"
     git push
   fi
 else
